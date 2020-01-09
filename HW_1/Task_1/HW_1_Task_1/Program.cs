05893f290eac2e2e3807c431d4f86993fc6f99f2
Andrey Lakusta <andrii.lakusta.pz.2017@lpnu.ua>
2020-01-10 00:24:59 +0200

    Homework done

---
+++ HW_1/Task_1/HW_1_Task_1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+            if (n >= 0 && n <= 109)
+            {
+                Console.WriteLine(Task(n));
+            }
+            else Console.WriteLine("Wrong number!");
+
+            Console.ReadKey();
+        }
+
+        private static long Task(int n)
+        {
+            if (n >= 0 && n <= 9) return n;
+
+            Stack<int> digits = GetDigitDividers(n);
+
+            if (digits.Pop() != 1) return -1;
+
+            return GetNumberFromDigits(digits);
+        }
+
+        private static Stack<int> GetDigitDividers(int n)
+        {
+            Stack<int> digits = new Stack<int>();
+
+            for (int i = 9; i > 1 && n > 1; i--)
+            {
+                while (n % i == 0)
+                {
+                    digits.Push(i);
+                    n /= i;
+                }
+            }
+
+            digits.Push(n);
+
+            return digits;
+        }
+
+        private static long GetNumberFromDigits(Stack<int> digits)
+        {
+            long res = 0;
+
+            while (digits.Count != 0)
+            {
+                res = res * 10 + digits.Pop();
+            }
+
+            return res;
+        }
+    }
+}
---
+++ HW_1/Task_2/HW_1_Task_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Task(n));
+
+            Console.ReadKey();
+        }
+        
+        public static long Task(int n)
+        {
+            return CombosOnBranch(n, 1) - 1;
+        }
+
+        private static long CombosOnBranch(int blocksLeft, int minHeight)
+        {
+            if (minHeight > blocksLeft) return 0;
+            if (minHeight == blocksLeft) return 1;
+
+            long combos = 0;
+            int currentHeight;
+            for (currentHeight = minHeight; currentHeight + 1 <= blocksLeft - currentHeight; currentHeight++)
+            {
+                long subBranchCombos = CombosOnBranch(blocksLeft - currentHeight, currentHeight + 1);
+                if (subBranchCombos > 0)
+                {
+                    combos += subBranchCombos;
+                }
+            }
+            if (blocksLeft > 0) combos++;
+
+            return combos;
+        }
+    }
+}
